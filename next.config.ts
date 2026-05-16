dc98b26d0a9241c9bf9ce9a815568fb85d6818f7
Tilak Popat <tilakpopat2007@gmail.com>
2026-05-16 11:42:03 +0530

    Force NEXT_PUBLIC_VERCEL_ENV to fix blob client routing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_VERCEL_ENV: process.env.VERCEL_ENV || 'production',
+  },
 };
 
 export default nextConfig;
